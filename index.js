e4f8d1bcfaa6f7cd7b1d282737d669afbcc7c4c0
Alexandra Davis <alex.davis806@gmail.com>
2022-03-16 17:06:34 -0700

    commit tree 3b40040143e26c01a7de976853cebf390638ac58

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = (name) => {
+    return cats.push(name);
+}
+
+console.log(destructivelyAppendCat("Ralph"))
+console.log(cats)
+
+const destructivelyPrependCat = name => {
+    return cats.unshift(name);
+}
+
+console.log(destructivelyPrependCat("Bob"));
+console.log(cats)
